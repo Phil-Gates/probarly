bc5997ad1a58eea93f131963586d94833af7e282
Phil-Gates <None>
2022-01-31 11:46:17 -0700

    Update to 1.0.1

--- probarly/__init__.py
+++ probarly/__init__.py
@@ -7,9 +7,7 @@ from probarly.probarly import (
     AnimationType,
     TaskType,
 )
-import probarly
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __author__ = "Phil-Gates"
 __credits__ = "me lol"
-__help__ = help(probarly.probarly)
