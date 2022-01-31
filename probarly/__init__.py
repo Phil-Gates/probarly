a4f5cdedb3b7d8f551a3db5b9089f0d2c717b770
Phil-Gates <None>
2022-01-30 20:58:46 -0700

    Initial commit.

---
+++ probarly/__init__.py
@@ -0,0 +1,5 @@
+from probarly.probarly import Task, Animation, BarAnimations, OtherAnimations, Progbar
+
+__version__ = "0.0.11"
+__author__ = "Phil-Gates"
+__credits__ = "me lol"
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="probarly",
+    version="0.0.11",
+    description="Create beautiful, heavily customizable progress bars... probarly",
+    url="http://github.com/phil-gates/probarly",
+    author="Phil",
+    author_email="mateopw10@gmail.com",
+    license="MIT",
+    packages=["probarly"],
+    zip_safe=False,
+)
